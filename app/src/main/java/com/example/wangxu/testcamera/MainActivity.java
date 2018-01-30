af58e409f37428a0f5becd10da83528ec8132449
wangxu93 <214047695@qq.com>
2018-01-30 14:38:42 +0800

    第一次提交+++++最新的

---
+++ app/src/main/java/com/example/wangxu/testcamera/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.wangxu.testcamera;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.cjt2325.cameralibrary.JCameraView;
+import com.cjt2325.cameralibrary.listener.ErrorListener;
+import com.cjt2325.cameralibrary.listener.JCameraListener;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private JCameraView jCameraView;
+    private String schemeSpecificPart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd HHmmss");
+        String takePhotofileName = df.format(new Date()) + ".jpg";
+        File homeFolder =
+                new File(Environment.getExternalStorageDirectory(), "chaoxing/chaoxingmobile/tempImages/");
+
+        Uri uri= Uri.fromFile(new File(homeFolder,takePhotofileName));
+
+        schemeSpecificPart = uri.getSchemeSpecificPart();
+
+        if (Build.VERSION.SDK_INT >= 19) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        } else {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(option);
+        }
+        jCameraView = (JCameraView) findViewById(R.id.jcameraview);
+        jCameraView.setDuration(3 *1000);
+
+//设置视频保存路径
+        jCameraView.setSaveVideoPath(Environment.getExternalStorageDirectory().getPath() + File.separator + "JCamera");
+
+//设置只能录像或只能拍照或两种都可以（默认两种都可以）
+        jCameraView.setFeatures(JCameraView.BUTTON_STATE_BOTH);
+
+
+//设置视频质量
+        jCameraView.setMediaQuality(JCameraView.MEDIA_QUALITY_MIDDLE);
+        jCameraView.setShowMode(JCameraView.SHOW_MODE_DEFAULT,5);
+
+//JCameraView监听
+        jCameraView.setErrorLisenter(new ErrorListener() {
+            @Override
+            public void onError() {
+                //打开Camera失败回调
+                Log.i("CJT", "open camera error");
+            }
+            @Override
+            public void AudioPermissionError() {
+                //没有录取权限回调
+                Log.i("CJT", "AudioPermissionError");
+            }
+        });
+
+        jCameraView.setJCameraLisenter(new JCameraListener() {
+            @Override
+            public void captureSuccess(Bitmap bitmap) {
+                //获取图片bitmap
+                Log.i("JCameraView", "bitmap = " + bitmap.getWidth());
+
+            }
+
+            @Override
+            public void recordSuccess(String url, Bitmap firstFrame) {
+                //获取视频路径
+                Log.i("CJT", "url = " + url);
+            }
+            @Override
+            public void quit() {
+                //退出按钮
+                MainActivity.this.finish();
+            }
+
+            @Override
+            public void captureSuccess(List<Uri> bitmaps) {
+                for (Uri bitmap : bitmaps) {
+                    Toast.makeText(MainActivity.this, bitmap.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        jCameraView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        jCameraView.onPause();
+    }
+}
---
+++ camera/src/main/java/com/cjt2325/cameralibrary/listener/JCameraListener.java
@@ -0,0 +1,26 @@
+package com.cjt2325.cameralibrary.listener;
+
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+import java.util.List;
+
+/**
+ * =====================================
+ * 作    者: 陈嘉桐
+ * 版    本：1.1.4
+ * 创建日期：2017/4/26
+ * 描    述：
+ * =====================================
+ */
+public interface JCameraListener {
+
+    void captureSuccess(Bitmap bitmap);
+
+    void recordSuccess(String url, Bitmap firstFrame);
+
+    void quit();
+
+    void captureSuccess(List<Uri> bitmaps);
+
+}
