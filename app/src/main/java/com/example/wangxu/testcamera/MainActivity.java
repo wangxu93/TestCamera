2f6fd6687132f635dd51dca3322b3addc1eee807
wangxu93 <214047695@qq.com>
2018-08-20 18:49:38 +0800

    修改配置

--- app/src/main/java/com/example/wangxu/testcamera/MainActivity.java
+++ app/src/main/java/com/example/wangxu/testcamera/MainActivity.java
@@ -132,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,SendedActivity.class));
+                startActivity(new Intent(MainActivity.this,MyBandActivity.class));
             }
         });
 
