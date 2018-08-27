d984217d68cd18ce3fcce1411d7b1fb409e47d23
wangxu93 <214047695@qq.com>
2018-08-27 17:58:39 +0800

    解决bug

--- camera/src/main/java/com/chaoxing/camera/util/CameraParamUtil.java
+++ camera/src/main/java/com/chaoxing/camera/util/CameraParamUtil.java
@@ -144,33 +144,38 @@ public class CameraParamUtil {
     }
 
     public int getCameraDisplayOrientation(Context context, int cameraId) {
-        android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
-        android.hardware.Camera.getCameraInfo(cameraId, info);
-        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-        int rotation = wm.getDefaultDisplay().getRotation();
-        int degrees = 0;
-        switch (rotation) {
-            case Surface.ROTATION_0:
-                degrees = 0;
-                break;
-            case Surface.ROTATION_90:
-                degrees = 90;
-                break;
-            case Surface.ROTATION_180:
-                degrees = 180;
-                break;
-            case Surface.ROTATION_270:
-                degrees = 270;
-                break;
+        try {
+            android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+            android.hardware.Camera.getCameraInfo(cameraId, info);
+            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            int rotation = wm.getDefaultDisplay().getRotation();
+            int degrees = 0;
+            switch (rotation) {
+                case Surface.ROTATION_0:
+                    degrees = 0;
+                    break;
+                case Surface.ROTATION_90:
+                    degrees = 90;
+                    break;
+                case Surface.ROTATION_180:
+                    degrees = 180;
+                    break;
+                case Surface.ROTATION_270:
+                    degrees = 270;
+                    break;
+            }
+            int result;
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                result = (info.orientation + degrees) % 360;
+                result = (360 - result) % 360;   // compensate the mirror
+            } else {
+                // back-facing
+                result = (info.orientation - degrees + 360) % 360;
+            }
+            return result;
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        int result;
-        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-            result = (info.orientation + degrees) % 360;
-            result = (360 - result) % 360;   // compensate the mirror
-        } else {
-            // back-facing
-            result = (info.orientation - degrees + 360) % 360;
-        }
-        return result;
+        return 90;
     }
 }
